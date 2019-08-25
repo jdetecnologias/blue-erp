c66119be7d0f2f973b948c9bbb624934729a4bcd
juliocesar.alves <juliowebdesigner@hotmail.com>
2019-08-25 10:57:45 +0000

    issue15 #15 - corrigido o layout do estoque

--- src/pages/estoque/estoque.container.js
+++ src/pages/estoque/estoque.container.js
@@ -4,15 +4,18 @@ import {bindActionCreators} from 'redux'
 import {importarEstoque,setEstoque} from  './estoque.container.action'
 
 import {importarProdutos} from './cadastrar.produto.action'
-import {JoinArray} from '../../common/operator/funcoes'
+import {JoinArray, controlarTamanhoTela as CTT, obterTamanhoGrid as OTG} from '../../common/operator/funcoes'
 import {Input,Form} from '../../common/layout/form'
 import Grid from '../../common/layout/grid'
 import Page from '../../common/layout/page'
+import If from '../../common/operator/if'
 
 
 class Estoque extends React.Component{
 	constructor(props){
 		super(props)
+		this.state = {tamanhoTela:window.innerWidth}
+		CTT(this)
 	}
 	componentWillMount(){
 		this.props.importarEstoque()	
@@ -22,32 +25,61 @@ class Estoque extends React.Component{
 		const estoque = JoinArray(this.props.estoque.estoque,'codigo',this.props.produtos,'_id')
 		
 		return (
-			<Page cols='11' title='Visualizar estoque'>
-				<Grid cols='12 10 8'  className='offset-sm-1 offset-md-2'>
+			<Page cols='12 12 12 11' title='Visualizar estoque'>
+				<Grid cols='12' >
 					<table className='table text-center'>
-						<tr className='bg-secondary text-white'>
-							<td>Cod.</td>
-							<td>Produto</td>
-							<td>qtd Total</td>
-							<td>qtd Disponível</td>		
-							<td>qtd Alocada</td>
+						<If test={this.state.tamanhoTela > OTG('xs')}>
+							<tr className='bg-secondary text-white'>
+								<td>Cod.</td>
+								<td>Produto</td>
+								<td>qtd Total</td>
+								<td>qtd Disponível</td>		
+								<td>qtd Alocada</td>
 
-						</tr>
-						{
-								 estoque.map(item=>{
-									 if(item.join){
-										return(
-											<tr>
-												<td>{item.join.codigo}</td>
-												<td>{item.join.descricaoCompleta}</td>
-												<td>{item.qtdTotal}</td>
-												<td>{item.qtdDisponivel}</td>							
-												<td>{item.qtdAlocada}</td>
-											</tr>
-										)
-									 }
-								})
-						}
+							</tr>
+							{
+									 estoque.map(item=>{
+										 if(item.join){
+											return(
+												<tr>
+													<td>{item.join.codigo}</td>
+													<td>{item.join.descricaoCompleta}</td>
+													<td>{item.qtdTotal}</td>
+													<td>{item.qtdDisponivel}</td>							
+													<td>{item.qtdAlocada}</td>
+												</tr>
+											)
+										 }
+									})
+							}
+						</If>
+						<If test={this.state.tamanhoTela <= OTG('xs')}>
+							{
+									 estoque.map((item,key)=>{
+										 if(item.join){
+											return(
+												<tr className={key%2 == 0 ? 'bg-secondary text-white' : 'bg-light text-dark'}>
+													<tr>
+														<td>{item.join.codigo}</td>
+														<td colspan='2'>{item.join.descricaoCompleta}</td>
+													</tr>
+													<tr>
+														<td>qtd Total</td>
+														<td>qtd Disponível</td>		
+														<td>qtd Alocada</td>												
+													</tr>
+													<tr>
+														<td>{item.qtdTotal}</td>
+														<td>{item.qtdDisponivel}</td>							
+														<td>{item.qtdAlocada}</td>
+													</tr>
+												</tr>
+											)
+										 }
+									})
+							}
+						</If>
+
 					</table>
 				</Grid>
 			</Page>
